dc3a10c23c94ed59c9777e48228dea4202359bf9
Yousquid <ly2456@nyu.edu>
2024-12-09 11:44:17 +0800

    1111

--- Assets/Scripts/Enemies/Crawler.cs
+++ Assets/Scripts/Enemies/Crawler.cs
@@ -4,22 +4,24 @@ using UnityEngine;
 
 public class Crawler : MonoBehaviour
 {
-    public float speed = 2f;
+    public float speed = 0.25f;
 
-    public float inFrontDistanceCheck = 0.5f;
-    public float groundDistanceCheck = 0.5f;
+    public float inFrontDistanceCheck = 0.2f;
+    public float groundDistanceCheck = 0.2f;
     public LayerMask groundCheckMask;
-    private bool isResting = true;
-
     private Animator animator;
     private Enemy enemyScript;
     private Rigidbody2D rb;
     private SpriteRenderer spriteRenderer;
 
-    private Vector2 direction = Vector2.right;
-    private bool isWallInFront = false;
-    private bool isGroundInFront = false;
+    private Vector2 direction;
+    public bool isWallInFront = false;
+    public bool isGroundInFront = false;
+    public string leftOrRight;
 
+    public float turnTimer = 0.05f;
+    public float tick = 0f;
+    public bool hasTurned = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -28,25 +30,41 @@ public class Crawler : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
         spriteRenderer = GetComponent<SpriteRenderer>();
-
-        SetIsFacingRight((Random.value < 0.5));
+        DirectionDetection();
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
+
         UpdateGroundChecks();
 
         //move the worm
         transform.Translate(direction * speed * Time.deltaTime);
 
         //turn around the worm
-        if (isWallInFront || !isGroundInFront)
-        {
-            transform.Rotate(0, 0, -90);
-            direction = transform.right; // Update direction
-        }
 
+        TurnTimer();
+
+    }
+
+    private void Update()
+    {
+        TurnTimer();
+    }
+
+    void DirectionDetection()
+    {
+        if (this.transform.position.x > 0)
+        {
+            leftOrRight = "right";
+            direction = Vector2.left;
+        }
+        if (this.transform.position.x < 0)
+        {
+            leftOrRight = "left";
+            direction = Vector2.right;
+        }
     }
 
     public void TakeStompDamage(int amount)
@@ -62,12 +80,62 @@ public class Crawler : MonoBehaviour
 
     private void UpdateGroundChecks()
     {
-        isWallInFront = Physics2D.Raycast(transform.position, Vector2.right * direction, inFrontDistanceCheck, groundCheckMask);
-        isGroundInFront = Physics2D.Raycast(transform.position + Vector3.right * 1f * inFrontDistanceCheck, Vector2.down, groundDistanceCheck, groundCheckMask);
+        RaycastHit2D onGroundRay;
+        RaycastHit2D infrontWallRay;
+      
+        
+        onGroundRay = Physics2D.Raycast(transform.position, transform.TransformDirection(Vector2.down), groundDistanceCheck, groundCheckMask);
+        
+        infrontWallRay = Physics2D.Raycast(transform.position, direction, inFrontDistanceCheck, groundCheckMask);
+
+        if (!onGroundRay && !hasTurned)
+        {
+            NoGroundTurnCrawler();
+            hasTurned = true;
+            tick = 0f;
+        }
+        if (infrontWallRay && !hasTurned)
+        {
+            InfrontWallTurnCrawler();
+            hasTurned = true;
+            tick = 0f;
+        }
     }
 
-    private void SetIsFacingRight(bool newIsFacingRight)
+    private void NoGroundTurnCrawler()
     {
-        
+        if (leftOrRight == "left")
+        {
+            this.transform.eulerAngles += new Vector3(0, 0, -90);
+        }
+        if (leftOrRight == "right")
+        {
+            this.transform.eulerAngles += new Vector3(0, 0, 90);
+        }
     }
+
+    private void InfrontWallTurnCrawler()
+    {
+        if (leftOrRight == "left")
+        {
+            this.transform.eulerAngles += new Vector3(0, 0, 90);
+        }
+        if (leftOrRight == "right")
+        {
+            this.transform.eulerAngles += new Vector3(0, 0, -90);
+        }
+    }
+
+    void TurnTimer()
+    {
+        if (hasTurned)
+        {
+            tick += Time.deltaTime;
+            if (tick >= turnTimer)
+            {
+                hasTurned = false;
+            }
+        }
+    }
+
 }
